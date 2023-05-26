d8141e07fe28c2a284082d94208d16e901f2689c
kelver-web <kelverwt@gmail.com>
2023-05-26 13:25:17 -0300

    Creating table categories

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
+
 import UserController from "./app/controllers/UserController"
 import SessionController from "./app/controllers/SessionController"
 import ProductController from "./app/controllers/ProductController"
+import CategoryController from "./app/controllers/CategoryController"
 
 import multer from "multer"
 import multerConfig from "./config/multer"
@@ -19,5 +21,9 @@ routes.use(authMiddlewareToken)
 
 routes.post("/products", upload.single("file"), ProductController.store)
 routes.get("/products", ProductController.index)
+
+routes.post("/categories", CategoryController.store)
+routes.get("/categories", CategoryController.index)
+
 
 export default routes
