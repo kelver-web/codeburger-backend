767c50966b4aa1443d5693ffc21b61a2d38fd510
kelver-web <kelverwt@gmail.com>
2023-05-26 14:23:10 -0300

    Creating relationship in code

--- src/database/migrations/20230526164756-create-category-column.js
+++ src/database/migrations/20230526164756-create-category-column.js
@@ -5,11 +5,11 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("products", "category_id", {
       type: Sequelize.INTEGER,
-      references: {model: "categories", key: "id"},
+      references: { model: "categories", key: "id" },
       onUpdate: "CASCADE",
       onDelete: "SET NULL",
       allowNull: true,
-     })
+    })
   },
 
   async down(queryInterface, Sequelize) {
